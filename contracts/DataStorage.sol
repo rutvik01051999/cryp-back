e6130d0aa7dea0c82cab3dbd71bb7bab3d918467
rutvik <rutvik.mohite@bytestechnolab.com>
2024-09-30 18:23:38 +0530

    fdfdf

--- contracts/DataStorage.sol
+++ contracts/DataStorage.sol
@@ -1,18 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DataStorage {
-    string private data;
-    bytes4 public storedData;
+// contract DataStorage {
+//     string private data;
+//     bytes4 public storedData;
 
-    function setData(string calldata _data) external {
+//     function setData(string calldata _data) external {
+//         data = _data;
+//     }
+
+//     function getData() external view returns (string memory) {
+//         return data;
+//     }
+//     function retrieveData() public view returns (bytes4) {
+//         return storedData;
+//     }
+// }
+
+contract DataStorage {
+    uint256 public data;
+    string public imageHash;
+
+    function setData(uint256 _data) public {
         data = _data;
     }
 
-    function getData() external view returns (string memory) {
+    function retrieveData() public view returns (uint256) {
         return data;
     }
-    function retrieveData() public view returns (bytes4) {
-        return storedData;
+
+    function setImageHash(string memory _imageHash) public {
+        imageHash = _imageHash;
+    }
+
+    function getImageHash() public view returns (string memory) {
+        return imageHash;
     }
 }
